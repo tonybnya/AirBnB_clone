64ee55c86beaba606202c230db940d10e8491057
Tony <nya.tony2010@gmail.com>
2023-02-12 19:30:20 +0100

    update

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -1,1 +1,60 @@
 #!/usr/bin/python3
+# test_file_storage.py
+# Author: @tonybnya
+"""
+This is a test function for the File Storage Engine.
+"""
+import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test class for FileStorage class."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+        self.fstorage = FileStorage()
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+    def test_all(self):
+        """Tests the all method returns an empty dictionary."""
+        self.assertEqual(self.fstorage.all(), {})
+
+    def test_new(self):
+        """Tests the new method sets a new object in __objects."""
+        model = BaseModel()
+        model.id = "1"
+        model.name = "Test"
+        self.fstorage.new(model)
+        self.assertEqual(len(self.fstorage.all()), 1)
+
+    def test_save(self):
+        """Tests the save method serializes __objects to the JSON file."""
+        model = BaseModel()
+        model.id = "1"
+        model.name = "Test"
+        self.fstorage.new(model)
+        self.fstorage.save()
+        with open("file.json") as file_obj:
+            self.assertTrue("BaseModel.1" in json.load(file_obj))
+
+    def test_reload(self):
+        """Tests the reload method deserializes the JSON file to __objects."""
+        model = BaseModel()
+        model.id = "1"
+        model.name = "Test"
+        self.fstorage.new(model)
+        self.fstorage.save()
+        self.fstorage.reload()
+        self.assertEqual(len(self.fstorage.all()), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,8 +5,9 @@
 This is a test function for the base class model.
 """
 import unittest
+from uuid import uuid4
 from datetime import datetime
-from AirBnB_clone.models.base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
@@ -14,28 +15,60 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """Set up a BaseModel instance for all the tests."""
-        self.base_model = BaseModel()
+        self.model = BaseModel()
 
     def test_init(self):
         """Test the initialization of an instance."""
-        self.assertIsInstance(self.base_model, BaseModel)
-        self.assertIsInstance(self.base_model.id, str)
-        self.assertIsInstance(self.base_model.created_at, datetime)
-        self.assertIsInstance(self.base_model.updated_at, datetime)
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
+
+    def test_id_is_a_uuid(self):
+        """Test if the 'id' attribute is a UUID."""
+        self.assertIsInstance(uuid4(hex=self.model.id), uuid4)
+
+    def test_created_and_updated_at_are_equal(self):
+        """Test if the 'created_at' and 'updated_at' are equal."""
+        self.assertEqual(self.model.created_at, self.model.updated_at)
 
     def test_str(self):
         """Test the string representation of an instance."""
-        self.assertIsInstance(str(self.base_model), str)
+        self.assertIsInstance(str(self.model), str)
 
     def test_save(self):
         """Test the save method updating the 'updated_at' attribute."""
-        updated_at = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(updated_at, self.base_model.updated_at)
+        updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(updated_at, self.model.updated_at)
 
     def test_to_dict(self):
-        """Test the to_dict method returning a dict representation."""
-        self.assertIsInstance(self.base_model.to_dict(), dict)
+        """Test the to_dict returns a dict representation."""
+        self.assertIsInstance(self.model.to_dict(), dict)
+
+    def test_to_dict_created_at_is_str(self):
+        """Test if the 'created_at' value in the dictionary
+        returned by 'to_dict' is a string.
+        """
+        self.assertIsInstance(self.model.to_dict()["created_at"], str)
+
+    def test_to_dict_updated_at_is_str(self):
+        """Test if the 'updated_at' value in the dictionary
+        returned by 'to_dict' is a string.
+        """
+        self.assertIsInstance(self.model.to_dict()["updated_at"], str)
+
+    def test_to_dict_class_key_exists(self):
+        """Test if the '__class__' key exists in the dictionary
+        returned by 'to_dict'.
+        """
+        self.assertIn("__class__", self.model.to_dict())
+
+    def test_to_dict_class_value_is_str(self):
+        """Test if the value of the '__class__' key in the dictionary
+        returned by 'to_dict' is a string.
+        """
+        self.assertIsInstance(self.model.to_dict()["__class__"], str)
 
 
 if __name__ == '__main__':
