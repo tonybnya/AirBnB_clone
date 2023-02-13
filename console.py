1f41d16f6150c7a8c357fdf171bb5066592e77e4
Tony <nya.tony2010@gmail.com>
2023-02-13 13:58:11 +0100

    update

--- console.py
+++ console.py
@@ -9,12 +9,6 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 def parse(line):
