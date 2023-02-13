ac3a7d32c8092a181394a83d5c034c99546bdf98
Tony <nya.tony2010@gmail.com>
2023-02-13 07:16:54 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,6 @@
 """
 Special magic file for the models package.
 """
-# from models.engine.file_storage import FileStorage
-
-# storage = FileStorage()
-# storage.reload()
 from models.engine import file_storage
 
 storage = file_storage.FileStorage()
