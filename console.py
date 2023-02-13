9068f165e2cecdbeedfea55d6ca37080eac56551
Tony <nya.tony2010@gmail.com>
2023-02-13 20:09:04 +0100

    update

--- console.py
+++ console.py
@@ -10,6 +10,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 def parse(line):
@@ -50,10 +55,10 @@ class HBNBCommand(cmd.Cmd):
 
     # Interactive Mode
     prompt = "(hbnb) "
-    __classes = {"BaseModel", "User"}
-    # __classes = {
-    #     "BaseModel", "User", "State", "City", "Place", "Amenity", "Review"
-    # }
+    __classes = {
+        "BaseModel", "User", "State", "City", "Place", "Amenity", "Review"
+    }
+    # __classes = {"BaseModel", "User"}
 
     def emptyline(self):
         """
